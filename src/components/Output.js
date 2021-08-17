e92d3169816651719f9d170a7d4a04e4c66f2e25
Jared Donovan <jdono100@gmail.com>
2021-08-17 18:16:20 -0500

    output div styling

--- src/components/Output.js
+++ src/components/Output.js
@@ -1,7 +1,7 @@
 export default function Output({ red, green, blue }) {
   return(
-    <div className="flex justify-center container mx-auto my-5" id="outputdiv" style={{backgroundColor: `rgb(${red}, ${green}, ${blue})`}}>
-      <p className="text-center" id="outputtext">
+    <div className="flex justify-center mx-auto my-5 w-6/12 border-4 border-gray-800 rounded-md" id="outputdiv" style={{backgroundColor: `rgb(${red}, ${green}, ${blue})`, border: `1px 1px solid purple`}}>
+      <p className="text-center text-gray-800 font-bold text-xl" id="outputtext">
         Output:<br/>
         R - {red}<br/>
         G - {green}<br/>
